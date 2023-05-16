ae6a7c9811952c774ca9afd68d3d1690cd5ea434
zach7815 <53745970+zach7815@users.noreply.github.com>
2023-05-16 17:43:59 +0800

    feat
     installed lodedash escape function to escape user input

--- Backend/index.js
+++ Backend/index.js
@@ -4,15 +4,16 @@ import fs from 'fs';
 import bodyParser from 'body-parser';
 const jsonParser = bodyParser.json();
 import ejs from 'ejs';
-import path from 'path';
+import { body, validationResult } from 'express-validator';
+import pkg from 'lodash';
+const { escape } = pkg;
+
 import {
 	validateAllWords,
 	gridDifficulty,
 	main,
 } from './wordSearchGenerator.js';
-
 import { htmlToPDF } from './pdfCreation.js';
-import { log } from 'console';
 
 const port = 3000;
 
@@ -27,28 +28,35 @@ app.use(function (req, res, next) {
 	next();
 });
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
-
 app.post('/api/WordSearchData', jsonParser, (req, res) => {
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.status(422).json({ errors: errors.array() });
+	}
+
+	// Access the sanitized data
 	const { authorName, header, title, difficulty, words } = req.body;
-	const wordsArray = words.split(', ');
+	const rawWordsArray = words.split(', ');
+	const escapedUserDetails = [authorName, header, title, difficulty].map(
+		(field) => escape(field),
+	);
+	const escapedWords = rawWordsArray.map((word) => escape(word));
+
 	const gridDimensions = gridDifficulty[difficulty];
 
-	if (validateAllWords(wordsArray)) {
-		const wordsearchGrid = main(wordsArray, gridDimensions);
+	if (validateAllWords(escapedWords)) {
+		const wordsearchGrid = main(escapedWords, gridDimensions);
 		const template = fs.readFileSync('./template.ejs', 'utf-8');
 		const data = {
-			title: title,
-			authorName: authorName,
+			authorName: escapedUserDetails[0],
+			header: escapedUserDetails[1],
+			title: escapedUserDetails[2],
 			wordSearchData: wordsearchGrid,
-			words: wordsArray,
-			level: difficulty,
+			words: escapedWords,
+			level: escapedUserDetails[3],
 		};
 
-		const html = ejs.render(template, data);
-		// console.log(html);
 		const fileName = `${title}.html`;
 		try {
 			fs.writeFileSync(`./htmlTemplates/${title}.html`, html, 'utf8');
