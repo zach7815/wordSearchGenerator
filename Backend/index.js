bd5b1e9f58425a4867ceb67bb5d3fd4ad3f939cb
zachariahkozlowski-best <mcfeegles@gmail.com>
2024-03-15 11:59:07 +0800

    chore: started conversion over to Typescript

--- Backend/index.js
+++ Backend/index.js
@@ -4,89 +4,94 @@ import fs from 'fs';
 import bodyParser from 'body-parser';
 const jsonParser = bodyParser.json();
 import ejs from 'ejs';
-import { validationResult } from 'express-validator';
+// import { validationResult } from 'express-validator';
 import pkg from 'lodash';
 const { escape } = pkg;
 
 import {
-	validateAllWords,
-	gridDifficulty,
-	main,
+  validateAllWords,
+  gridDifficulty,
+  main,
 } from './word-search-generator.js';
 import { htmlToPDF } from './pdfCreation.js';
 
 const port = 3000;
 
 app.use(function (req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept',
-	);
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
 });
 
 app.post('/api/WordSearchData', jsonParser, (req, res) => {
-	const errors = validationResult(req);
+  // const errors = validationResult(req);
 
-	if (!errors.isEmpty()) {
-		return res.status(422).json({ errors: errors.array() });
-	}
+  // if (!errors.isEmpty()) {
+  //   return res.status(422).json({ errors: errors.array() });
+  // }
 
-	// Access the sanitized data
-	const { authorName, header, title, difficulty, words } = req.body;
-	const rawWordsArray = words.split(', ');
-	const escapedUserDetails = [authorName, header, title, difficulty].map(
-		(field) => escape(field),
-	);
-	const escapedWords = rawWordsArray.map((word) => escape(word));
+  // Access the sanitized data
+  const { authorName, header, title, difficulty, words } = req.body;
+  const rawWordsArray = words.split(', ');
 
-	const gridDimensions = gridDifficulty[difficulty];
+  console.log(rawWordsArray);
+  const escapedUserDetails = [authorName, header, title, difficulty].map(
+    (field) => escape(field)
+  );
+  const escapedWords = rawWordsArray.map((word) => escape(word));
 
-	if (validateAllWords(escapedWords)) {
-		const wordsearchGrid = main(escapedWords, gridDimensions);
-		const wordSearchTemplate = fs.readFileSync('./wordSearch.ejs', 'utf-8');
-		const answersTemplate = fs.readFileSync('./answers.ejs', 'utf-8');
-		const data = {
-			authorName: escapedUserDetails[0],
-			header: escapedUserDetails[1],
-			title: escapedUserDetails[2],
-			wordSearchData: wordsearchGrid.filledGrid,
-			answers: wordsearchGrid.answers,
-			words: escapedWords,
-			level: escapedUserDetails[3],
-		};
-		const htmlWordSearch = ejs.render(wordSearchTemplate, data);
-		const htmlAnswerGrid = ejs.render(answersTemplate, data);
-		const wordSearchFileName = `${data.title}.html`;
-		const answerSheetFileName = `${data.title}_answers.html`;
-		try {
-			fs.writeFileSync(
-				`./htmlTemplates/${wordSearchFileName}`,
-				htmlWordSearch,
-				'utf8',
-			);
-			fs.writeFileSync(
-				`./htmlTemplates/${answerSheetFileName}`,
-				htmlAnswerGrid,
-				'utf8',
-			);
-			htmlToPDF(`./htmlTemplates/${wordSearchFileName}`);
-			htmlToPDF(`./htmlTemplates/${answerSheetFileName}`);
-		} catch (error) {
-			console.error;
-		} finally {
-			console.log('done');
-		}
-	} else {
-		console.log('the words used are invalid');
-	}
+  const gridDimensions = gridDifficulty[difficulty];
 
-	res.end();
+  if (validateAllWords(escapedWords)) {
+    const wordsearchGrid = main(escapedWords, gridDimensions);
+    const wordSearchTemplate = fs.readFileSync(
+      'word-search-template.ejs',
+      'utf-8'
+    );
+    const answersTemplate = fs.readFileSync('./answers.ejs', 'utf-8');
+    const data = {
+      authorName: escapedUserDetails[0],
+      header: escapedUserDetails[1],
+      title: escapedUserDetails[2],
+      wordSearchData: wordsearchGrid.filledGrid,
+      answers: wordsearchGrid.answers,
+      words: escapedWords,
+      level: escapedUserDetails[3],
+    };
+    const htmlWordSearch = ejs.render(wordSearchTemplate, data);
+    const htmlAnswerGrid = ejs.render(answersTemplate, data);
+    const wordSearchFileName = `${data.title}.html`;
+    const answerSheetFileName = `${data.title}_answers.html`;
+    try {
+      fs.writeFileSync(
+        `./htmlTemplates/${wordSearchFileName}`,
+        htmlWordSearch,
+        'utf8'
+      );
+      fs.writeFileSync(
+        `./htmlTemplates/${answerSheetFileName}`,
+        htmlAnswerGrid,
+        'utf8'
+      );
+      htmlToPDF(`./htmlTemplates/${wordSearchFileName}`);
+      htmlToPDF(`./htmlTemplates/${answerSheetFileName}`);
+    } catch (error) {
+      console.error;
+    } finally {
+   
+    }
+  } else {
+    console.log('the words used are invalid');
+  }
+
+  res.end();
 });
 
 app.listen(port, () => {
-	console.log(`app is listening on port:${port}`);
+  console.log(`app is listening on port:${port}`);
 });
